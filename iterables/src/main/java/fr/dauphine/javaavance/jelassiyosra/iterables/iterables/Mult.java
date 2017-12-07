7915a84fb721a881afcad65fcca5f219b0fa4f3f
Yosrajel <32701896+Yosrajel@users.noreply.github.com>
2017-12-07 22:53:02 +0100

    Commit v2

--- iterables/src/main/java/fr/dauphine/javaavance/jelassiyosra/iterables/iterables/Mult.java
+++ iterables/src/main/java/fr/dauphine/javaavance/jelassiyosra/iterables/iterables/Mult.java
@@ -1,88 +1,112 @@
 package fr.dauphine.javaavance.jelassiyosra.iterables.iterables;
 
-import java.util.AbstractList;
-import java.util.AbstractSequentialList;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+	import java.util.AbstractList;
+	import java.util.AbstractSequentialList;
+	import java.util.ArrayList;
+	import java.util.LinkedList;
+	import java.util.List;
+	import java.util.ListIterator;
 
-public class Mult {
+	public class Mult {
+			
+		public static List<Integer> mult(final int j,final List<Integer> l3){
+			
+		if (l3 instanceof AbstractList)
+		{return new AbstractList <Integer>() {
+
+			@Override
+			public Integer get(int index) {
+				return (j*l3.get(index));
+			}
+
+			@Override
+			public int size() {
+				return l3.size();
+			}
+
+			@Override
+			public ListIterator<Integer> listIterator(int index) {
+				ListIterator<Integer> li=l3.listIterator();
+
+			    return li;
+			  		}};}
 		
-	public static AbstractSequentialList <Integer> mult(final int j,final List<Integer> l3){		
-	return new AbstractSequentialList <Integer>() {
-
-		@Override
-		public Integer get(int index) {
-			return j*l3.get(index);
-		}
-
-		@Override
-		public int size() {
-			return l3.size();
-		}
-
-		@Override
-		public ListIterator<Integer> listIterator(int index) {
-			ListIterator<Integer> li=l3.listIterator();
-
-		    return li;
-		  		}};}
-         	
-	public static void main(String[] args) {
-		ArrayList<Integer> al = new ArrayList<>();
-		for (int i = 0; i < 1000000; i++) {
-		al.add(i);
-		}
-		long t0 = System.nanoTime();
-		List<Integer> ret = Mult.mult(2, al);
-		long sum=0;
-		for(int val : ret) {
-		sum+=val/2;
-		}
-		System.out.println((System.nanoTime() - t0));
-		LinkedList<Integer> ll = new LinkedList<>();
-		for (int i = 0; i < 1000000; i++) {
-		ll.add(i);
-		}
-		t0 = System.nanoTime();
-	    sum=0;
-		ret = Mult.mult(2, ll);
-		for(int val : ret) {
-		sum+=val/2;
-		}
-		System.out.println((System.nanoTime() - t0));}
-}
-//Question2:
+		else 
+		{return new AbstractSequentialList<Integer>() {
 	
-//	public static List<Integer> mult(final int j,final List<Integer> l3){		
-//	return new AbstractList<Integer>() {
-//
-//		@Override
-//		public Integer get(int index) {
-//			return j*l3.get(index);
-//		}
-//
-//		@Override
-//		public int size() {
-//			return l3.size();
-//		}};}		
+			@Override
+			public Integer get(int index) {
+				return (j*l3.get(index)-5);
+			}
 	
-	
-//Question 1
-	
-//public static List<Integer> mult(final int j,final List<Integer> l3){
-//		
-//		List<Integer> l=new ArrayList<Integer>();
-//		
-//		for (int i=0;i<l3.size();i++) {
-//			l.add(l3.get(i)*j);
-//		}
-//		return l;
-//	}	
+			@Override
+			public int size() {
+				return l3.size();
+			}
 
+			@Override
+			public ListIterator<Integer> listIterator(int index) {
+				ListIterator<Integer> li=l3.listIterator();
 
+			    return li;
+			  		}};}}		
 		
 	
+	         	
+		public static void main(String[] args) {
+			ArrayList<Integer> al = new ArrayList<>();
+			for (int i = 0; i < 10; i++) {
+			al.add(i);
+			}
+			long t0 = System.nanoTime();
+			List<Integer> ret = Mult.mult(2, al);
+			System.out.println(ret);
+			long sum=0;
+			for(int val : ret) {
+			sum+=val/2;
+			}
+			System.out.println((System.nanoTime() - t0));
+			LinkedList<Integer> ll = new LinkedList<>();
+			for (int i = 0; i < 10; i++) {
+			ll.add(i);
+			}
+			t0 = System.nanoTime();
+			List<Integer> ret1 = Mult.mult(2, al);
+		    sum=0;
+			ret1 = Mult.mult(2, ll);
+			System.out.println(ret1);
+			/*for(int val : ret) {
+			sum+=val/2;
+			}*/
+			System.out.println((System.nanoTime() - t0));}}
+	
+//	//Question2:
+//		
+//		public static List<Integer> mult(final int j,final List<Integer> l3){		
+//		return new AbstractList<Integer>() {
+//	
+//			@Override
+//			public Integer get(int index) {
+//				return j*l3.get(index);
+//			}
+//	
+//			@Override
+//			public int size() {
+//				return l3.size();
+//			}};}		
+//		
+		
+	//Question 1
+		
+//	public static List<Integer> mult(final int j,final List<Integer> l3){
+//			
+//			List<Integer> l=new ArrayList<Integer>();
+//			
+//			for (int i=0;i<l3.size();i++) {
+//				l.add(l3.get(i)*j);
+//			}
+//			return l;
+//		}	
+//	}
 
-
+	
--- iterables/src/main/java/fr/dauphine/javaavance/jelassiyosra/iterables/iterables/Panel.java
+++ iterables/src/main/java/fr/dauphine/javaavance/jelassiyosra/iterables/iterables/Panel.java
@@ -1,91 +1,109 @@
 package fr.dauphine.javaavance.jelassiyosra.iterables.iterables;
 
-import java.util.AbstractList;
-import java.util.Iterator;
-import java.util.List;
 
-class Panel    {
-	
-    //la classe AbstractList permet un acces direct avec indice 
-	// les méthodes abstraites à implémenter sont get() et size()
-	
-	public static List<Integer> panel(final int a,final int b){
-	    return new AbstractList<Integer>() {
-			@Override
-			public Integer get(int index) {
-				return a+index;
-			}
 
-			@Override
-			public int size() {
-				return b-a+1;
-			}};}
-	    	
-  public static void main(String[] args) {
-	  List<Integer> l = panel(3,6);
-	   for(int i:l) {
-	   System.out.println(i);
-	   }
-	  System.out.println(l.get(1)); 
+	import java.util.AbstractList;
+	import java.util.Iterator;
+	import java.util.List;
+import java.util.NoSuchElementException;
 
+	class Panel    {
+		
+	    //la classe AbstractList permet un acces direct avec indice 
+		// les méthodes abstraites à implémenter sont get() et size()
+		
+		public static List<Integer> panel(final int a,final int b){
+		    return new AbstractList<Integer>() {
+		    	
+		    	public Iterator<Integer> iterator() {
+		            return panel1(a,b);
+		       }
+		    	
+				@Override
+				public Integer get(int index) {
+					return a+index;
+				}
+
+				@Override
+				public int size() {
+					return b-a+1;
+				}};}
+		    	
+	  public static void main(String[] args) {
+		  try {
+		  List<Integer> l = panel(3,6);
+		   for(int i:l) {
+		   System.out.println(i); }
+		  System.out.println(l.get(1)); 
+		
+		  Iterator<Integer> tt=panel(2,2).iterator();
+          System.out.println(tt.next());
+          System.out.println(tt.next());}
+          catch  (NoSuchElementException e) {
+        	  System.out.println("Pas d'élément successeur");
+        	  
+          }
+	  }
 	
-}}
+	  
+		
+		//Pour pouvoir utiliser la "foreach" on doit implémenter l'interface
+//		//Iterable
+//	static Iterable<Integer> panel2(final int min,final int max){
+//	//
+//			return new Iterable<Integer>() {
+//			int start=min;
+//			int end=max;
+//			 public Iterator<Integer> iterator() {	
+//					return panel1(start,end);} };}
+		
+	static Iterator<Integer> panel1(final int a,final int b){
+		 
+			return new Iterator<Integer>() {
+			int i=a;
+			public boolean hasNext() {
+			if (this.i<=b) { 
+			return true;}
+			else {return false;}}
 	
-	//Pour pouvoir utiliser la "foreach" on doit implémenter l'interface
-	//Iterable
-//static Iterable<Integer> panel2(final int min,final int max){
-//
-//		return new Iterable<Integer>() {
-//		int start=min;
-//		int end=max;
-//		 public Iterator<Integer> iterator() {	
-//				return panel1(start,end);} };}
+			public Integer next() {
+			if (hasNext()){
+			return i++;} 
+			else {throw new NoSuchElementException("pas de next");}
+			}};}}
+	
+		
+		        	     
+//	    public static void main(String[] args) {
+//	    for(int i:panel2(1,5)) {
+//	     System.out.println(i);}}
+//		    }
+	
+	 
+//	static Iterator<Integer> panel1(int min,int max){
+//			
+//			return new Implemiterator(min,max);}
 //	
-//static Iterator<Integer> panel1(final int a,final int b){
-//	 
-//		return new Iterator<Integer>() {
-//		int i=a;
-//		public boolean hasNext() {
-//		if (this.i<=b) { 
-//		return true;}
-//		else {return false;}}
-//
-//		public Integer next() {
-//		if (hasNext()){
-//		return i++;} 
-//		else {throw new IllegalStateException("pas de next");}
-//		}};}
-//
-//	
-//	        	     
-//    public static void main(String[] args) {
-//    for(int i:panel2(1,5)) {
-//     System.out.println(i);}}
-//	    }
-//
-// 
-//static Iterator<Integer> panel1(int min,int max){
-//		
-//		return new Implemiterator(min,max);}
-//
-//static class Implemiterator implements Iterator<Integer> {
-//         int vald,valf;
-//         Implemiterator(int vald,int valf){
-//         this.vald=vald;
-//         this.valf=valf;  
-//          }
-//public boolean hasNext() {
-//	if (this.vald<=this.valf) { 
+//	static class Implemiterator implements Iterator<Integer> {
+//	         int vald,valf;
+//	         Implemiterator(int vald,int valf){
+//	         this.vald=vald;
+//	         this.valf=valf;  
+//	          }
+//	public boolean hasNext() {		
+//		if (this.vald<=this.valf) { 
 //	return true;}
 //	else {return false;}}
-//
-//
-//public Integer next() {
-// if (hasNext()){
-// return vald++;}
-// throw new IllegalStateException("pas de next");}}
-//
-//public static void main(String[] args) {
-//Iterator<Integer> it=panel1(1,5);
-// for(;it.hasNext();)
-// System.out.println(it.next()); }}
+//	
+//	
+//	public Integer next() {
+//	 if (hasNext()){
+//	 return vald++;}
+//	 throw new IllegalStateException("pas de next");}}
+	
+//	public static void main(String[] args) {
+//	Iterator<Integer> it=panel1(1,5);
+//	 for(;it.hasNext();)
+//	 System.out.println(it.next()); }}
+
+
